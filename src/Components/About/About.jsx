615a8603ff2a0d0097f073943571c3e388e7238b
jahime001 <jahimecamea@gmail.com>
2023-07-19 08:29:43 -0400

    about section done just need to add images

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,12 +5,47 @@ export default function About() {
   return (
     <div className="About">
       <div className="about-container">
-        <div>
-          <div className="about-sect-box"></div>
+        <div className="about-sect-cont">
+          <div className="about-sect-box">
+            <p className="about-text-left">
+              Ripple is a leading financial technology company dedicated to
+              empowering businesses financially. With our innovative solutions,
+              we strive to simplify and optimize financial operations, allowing
+              businesses to thrive in today's fast-paced economy. Our mission is
+              to provide cutting-edge tools and services that streamline
+              payments, enhance cash flow management, and unlock growth
+              opportunities for our clients.
+            </p>
+          </div>
           <div className="about-sect-box"></div>
         </div>
-        <div></div>
-        <div></div>
+        <div className="about-sect-cont">
+          <div className="about-sect-box"></div>
+          <div className="about-sect-box">
+            <p className="about-text-right">
+              At Ripple, we believe in the power of technology to revolutionize
+              the way businesses manage their finances. Our team of experts is
+              passionate about delivering secure, reliable, and user-friendly
+              solutions that enable businesses to focus on what they do best. By
+              harnessing the latest advancements in financial technology, we are
+              committed to helping businesses navigate financial challenges and
+              achieve their goals.
+            </p>
+          </div>
+        </div>
+        <div className="about-sect-cont">
+          <div className="about-sect-box">
+            <p className="about-text-left">
+              With a customer-centric approach, we prioritize the needs of our
+              clients and strive for excellence in everything we do. Our
+              dedicated support team is always available to provide personalized
+              assistance, ensuring a smooth and successful experience for our
+              valued customers. Join the Ripple community and discover the
+              difference our financial solutions can make for your business.
+            </p>
+          </div>
+          <div className="about-sect-box"></div>
+        </div>
       </div>
     </div>
   );
